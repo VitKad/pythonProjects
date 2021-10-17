e374c929c0fff78cbb9b05fe3ced0861fd55a7a6
Viktor Kadyko <victor_-kad@mail.ru>
2021-10-17 23:55:22 +0300

    add comments

--- salat/statistic.py
+++ salat/statistic.py
@@ -6,16 +6,16 @@ import matplotlib.pyplot as plt
 
 
 
-def calculateStatistic(text):
+def calculateStatistic(text):    #функция расчета частотного анализа
     buf=1072
-    words = [0] * 32
+    words = [0] * 32       #сооздание пустых массивов
     statistic = [0] * 32
     percents = [0] * 32
-    for i in range(0, 32):
+    for i in range(0, 32):   #цикл заполнения массива алфавита
         words[i]=chr(buf)
         buf+=1
 
-    for i in text:
+    for i in text:       #цикл подсчета букв в тексте
         if (ord(i)>=1040 and ord(i)<=1103):
             if (ord(i)>=1040 and ord(i)<=1071):
                 statistic[ord(i)-1040]+=1
@@ -23,13 +23,13 @@ def calculateStatistic(text):
                 statistic[ord(i)-1072]+=1
 
     count = 0
-    for i in range(0,32):
+    for i in range(0,32):    #подсчет количества букв в целом
         count+=statistic[i]
 
-    for i in range(0, 32):
+    for i in range(0, 32):     #заполнение массива с процентами
         percents[i]=(statistic[i]/count)*100
 
-    for i in range(31):
+    for i in range(31):    #сортировка всех массивов
         for j in range(31-i):
             if statistic[j] < statistic[j+1]:
                 statistic[j], statistic[j+1] = statistic[j+1], statistic[j]
@@ -37,22 +37,17 @@ def calculateStatistic(text):
                 percents[j], percents[j+1] = percents[j+1], percents[j]
 
 
-    plt.subplot(1, 2, 1)
+    plt.subplot(1, 2, 1)  #столбики
     plt.bar(words, statistic)
-    plt.title("Частотный анализ зашифрованного текста")
+    plt.title("Частотный анализ текста")
     plt.xlabel("Буквы")
     plt.ylabel("Количество")
-    #plt.show()
 
-    plt.subplot(1, 2, 2)
+    plt.subplot(1, 2, 2)  #круговая
     plt.title('Частотный анализ в процентах')
     plt.pie(percents,labels=words)
     plt.axis('equal')
     plt.show()
-
-   # print(statistic)
-   # print(words)
-   # print(percents)
 
     return statistic, words;
 
@@ -66,8 +61,7 @@ with open('example.txt') as f:
 
 statExamp, wordsExamp = calculateStatistic(example)
 
-print(statText)
-print(wordsText)
+
 print("Буквы, которые повторяются чаще всего")
 print("Для зашифрованного текста:", wordsText[0], " - ", statText[0], ", ", wordsText[1], " - ", statText[1], ", ", wordsText[2], " - ", statText[2], ". ")
 print("Для обычного текста:", wordsExamp[0], " - ", statExamp[0], ", ", wordsExamp[1], " - ", statExamp[1], ", ", wordsExamp[2], " - ", statExamp[2], ". ")
