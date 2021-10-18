79e388d17bb5c154de30dd46f2452a75657f7a08
Victor Kadyko <victor_-kad@mail.ru>
2021-10-18 22:28:36 +0300

    add percents, edit size and names

--- salat/statistic.py
+++ salat/statistic.py
@@ -1,12 +1,6 @@
 import matplotlib.pyplot as plt
 
-
-#words = [buf+1]*32
-#text = "ааАаабббввгдетЯы!,№%эюя"
-
-
-
-def calculateStatistic(text):    #функция расчета частотного анализа
+def calculateStatistic(text, c):    #функция расчета частотного анализа
     buf=1072
     words = [0] * 32       #сооздание пустых массивов
     statistic = [0] * 32
@@ -36,33 +30,42 @@ def calculateStatistic(text):    #функция расчета частотно
                 words[j], words[j+1] = words[j+1], words[j]
                 percents[j], percents[j+1] = percents[j+1], percents[j]
 
-
+    plt.figure(num='Частотный анализ', figsize=(16, 8))
     plt.subplot(1, 2, 1)  #столбики
     plt.bar(words, statistic)
-    plt.title("Частотный анализ текста")
+    if (c==1):
+        plt.title("Частотный анализ зашифрованного текста")
+    if (c==2):
+        plt.title("Частотный анализ текста из интернета")
     plt.xlabel("Буквы")
     plt.ylabel("Количество")
 
     plt.subplot(1, 2, 2)  #круговая
     plt.title('Частотный анализ в процентах')
-    plt.pie(percents,labels=words)
+    plt.pie(percents,labels=words, autopct='%1.0f%%', pctdistance=1.1, labeldistance=1.2)
     plt.axis('equal')
+
+
     plt.show()
 
-    return statistic, words;
+    return statistic, words, percents;
 
 with open('task.txt') as f:
     text = f.read()
 
-statText, wordsText = calculateStatistic(text)
+statText, wordsText, percentText = calculateStatistic(text, 1)
 
 with open('example.txt') as f:
     example = f.read()
 
-statExamp, wordsExamp = calculateStatistic(example)
+statExamp, wordsExamp, percentExamp = calculateStatistic(example, 2)
 
 
-print("Буквы, которые повторяются чаще всего")
-print("Для зашифрованного текста:", wordsText[0], " - ", statText[0], ", ", wordsText[1], " - ", statText[1], ", ", wordsText[2], " - ", statText[2], ". ")
-print("Для обычного текста:", wordsExamp[0], " - ", statExamp[0], ", ", wordsExamp[1], " - ", statExamp[1], ", ", wordsExamp[2], " - ", statExamp[2], ". ")
-print("Предпологаемый ключ: ", ord(wordsText[0])-ord(wordsExamp[0]), ', ',ord(wordsText[1])-ord(wordsExamp[1]), ', ',ord(wordsText[2])-ord(wordsExamp[2]))
+print("Количество букв, которые повторяются чаще всего")
+print("Для зашифрованного текста:", wordsText[0], " - ", statText[0], "(", float('{:.3f}'.format(percentText[0])), "% ) ",
+      ", ", wordsText[1], " - ", statText[1],"(", float('{:.3f}'.format(percentText[1])), "% ) ",
+      ", ", wordsText[2], " - ", statText[2],"(", float('{:.3f}'.format(percentText[2])), "% ) ", ". ")
+print("Для обычного текста:", wordsExamp[0], " - ", statExamp[0],"(", float('{:.3f}'.format(percentExamp[0])), "% ) ",
+      ", ", wordsExamp[1], " - ", statExamp[1], "(", float('{:.3f}'.format(percentExamp[1])), "% ) ",
+      ", ", wordsExamp[2], " - ", statExamp[2], "(", float('{:.3f}'.format(percentExamp[2])), "% ) ",". ")
+print("Предпологаемые ключи: ", ord(wordsText[0])-ord(wordsExamp[0]), ', ',ord(wordsText[1])-ord(wordsExamp[1]), ', ',ord(wordsText[2])-ord(wordsExamp[2]))
